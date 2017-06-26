7fcdb995c1adac033d3ce94bb1b2286f5bc6d8c6
miguel <miguelnetojet@hotmail.com>
2017-06-26 04:57:41 -0300

    cam e specmode

--- Public/sketch.js
+++ Public/sketch.js
@@ -1,18 +1,34 @@
 var socket;
 var playerList = [];
 var playerID = 1000;
+var zoom = 1;
+var quadr;
+specmode = false;
+var bola = new Bola();
+var xadjust = 0;
+var yadjust = 0;
 
-for (var i = 0; i < 8; i++) {
+for (var i = 0; i < 2; i++) {
   playerList[i] = new Player();
 }
 
 function setup() {
-  xw=windowWidth;
-  yw=windowHeight;
-  createCanvas(xw, yw);
+  createCanvas(windowWidth, windowHeight);
   socket = io.connect();
   background(55,0,30);
+  quadr = new Quadra();
+  xadjust = windowWidth/2 - quadr.sx/2;
+  yadjust = windowHeight/2 - quadr.sy/2;
 
+
+  socket.on('full', function() {
+    specmode = true;
+  });
+
+  socket.on('ball', function(b) {
+    bola.x = b.x;
+    bola.y = b.y;
+  });
 
   socket.on('new-player',function(ID) {
     if(playerID == 1000){
@@ -20,6 +36,7 @@ function setup() {
     }
     playerList[ID] = new Player();
   });
+
   socket.on('update', function(trespa, onlinePlayers) {
     if (playerList.length != 0) {
       for (var i = 0; i < onlinePlayers; i++) {
@@ -31,33 +48,86 @@ function setup() {
 }
 
 function draw() {
-  background(0);
+  createCanvas(windowWidth, windowHeight);
+  background(100, 200, 50);
+  quadr.draw();
+  bola.draw();
   for (var i = 0; i < playerList.length; i++) {
     playerList[i].draw();
   }
-  if (keyIsDown(LEFT_ARROW)){
-    socket.emit('joystick-mv-horz', playerID, -1);
+  if(!specmode){
+    controlpadfunc();
+  }else{
+    textSize('30');
+    fill(random(0,255), random(0,255), random(0,255));
+    text('spectating', windowWidth/2, windowHeight/2);
   }
-
-   if (keyIsDown(RIGHT_ARROW)){
-     socket.emit('joystick-mv-horz', playerID, 1);
-   }
-
-   if (keyIsDown(UP_ARROW)){
-     socket.emit('joystick-mv-vert', playerID, -1);
-   }
-
-   if (keyIsDown(DOWN_ARROW)){
-     socket.emit('joystick-mv-vert', playerID, 1);
-   }
-   socket.emit('request');
+  socket.emit('request');
 }
+
+
 function Player(){
+  this.x = -100;
+  this.y = -100;
+
+  this.draw = function() {
+    fill(0,0,0,50);
+    rect(this.x +xadjust,this.y-2+yadjust, -7, 14);
+    fill(19, 164, 190);
+    rect(this.x+xadjust, this.y+yadjust, 7, 14);
+    fill(105, 46, 10);
+    rect(this.x+xadjust, this.y+4+yadjust, 7, 5);
+
+  }
+}
+
+
+function Quadra(){
+  this.x = 0;
+  this.y = 0;
+  this.sx = 1400;
+  this.sy = 600;
+
+  this.draw = function(){
+    strokeWeight(2);
+    stroke(255);
+    fill(100, 150, 50);
+    rect(this.x+xadjust, this.y+yadjust, this.sx, this.sy);
+    rect(this.sx/4+this.x+xadjust, this.y+yadjust, this.sx/2, this.sy);
+    rect(this.sx/4+this.x+xadjust, this.y+yadjust, this.sx/2, this.sy/2);
+    //draw net
+    fill(0,0,0,50);
+    noStroke();
+    rect(this.x+this.sx/2-6+xadjust, yadjust-55, 4, 700);
+    fill(255);
+    rect(this.x+this.sx/2-2+xadjust, yadjust-50, 4, 700);
+  }
+}
+
+function Bola(){
   this.x = 0;
   this.y = 0;
 
   this.draw = function() {
-    fill(255);
-    rect(this.x, this.y, 10, 10);
+    fill(255, 255, 50);
+    rect(this.x+xadjust, this.y+yadjust, 6, 6);
+  }
+}
+
+function controlpadfunc() {
+  if (keyIsDown(LEFT_ARROW)){
+    socket.emit('joystick-mv-horz', playerID, -1);
+  }
+
+  if (keyIsDown(RIGHT_ARROW)){
+    socket.emit('joystick-mv-horz', playerID, 1);
+  }
+
+  if (keyIsDown(UP_ARROW)){
+    socket.emit('joystick-mv-vert', playerID, -1);
+  }
+
+  if (keyIsDown(DOWN_ARROW)){
+    socket.emit('joystick-mv-vert', playerID, 1);
   }
 }
--- index.js
+++ index.js
@@ -14,23 +14,59 @@ io.sockets.on('connection', newConnection);
 ///////////////////////////////////
 var playerArray = [];
 var onlinePlayers = 0;
+var ball;
+var lastouch = 100; //guarda o id do ultimo player a tocar na bola
 
 function newConnection(socket){
-	console.log('New Player Online, socket connection: ' + socket.id);
-  playerArray[onlinePlayers] = new Player();
-	socket.emit('new-player', onlinePlayers);
-	onlinePlayers++;
-  // socket.emit('mouse', datarray);
+	if(onlinePlayers <= 1){
+		console.log('New Player Online, socket connection: ' + socket.id);
+		ball = new Ball();
+		playerArray[onlinePlayers] = new Player();
+		socket.emit('new-player', onlinePlayers);
+		onlinePlayers++;
+	}else {
+		console.log("Rom is full");
+		socket.emit('full');
+	}
   socket.on('joystick-mv-vert', movev);
   socket.on('joystick-mv-horz', moveh);
 	socket.on('request', function() {
+		ballmove();
+		socket.emit('ball', ball);
 		socket.emit('update', playerArray, onlinePlayers);
 	});
 }
 
+function ballmove(){
+	for (var i = 0; i < onlinePlayers; i++) {
+		if(lastouch != i){
+			if(playerArray[i].x<ball.x && playerArray[i].y<ball.y){
+				if(playerArray[i].x+playerArray[i].width>ball.x){
+					if(playerArray[i].y+playerArray[i].height>ball.y){
+						ball.speedX = -1*ball.speedX*(Math.random());
+						ball.speedY = Math.random()-Math.random();
+						lastouch = i;
+					}
+				}
+			}
+		}
+	}
+	ball.x = ball.x+ball.speedX;
+	ball.y = ball.y+ball.speedY;
+}
+
 function Player(){
-  this.x = 500;
+  this.x = 500*(onlinePlayers+1);
   this.y = 500;
+	this.width = 100;
+	this.height = 100;
+}
+
+function Ball(){
+	this.x = 200;
+	this.y = 555;
+	this.speedX = 3;
+	this.speedY = 0;
 }
 
 function moveh(player, vx) {
